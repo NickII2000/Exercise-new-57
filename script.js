b46dcbc5e0b5caebf83cc3e26c27ba92489b7ab8
Nick <Nick_II2000@mail.ru>
2022-05-23 21:25:37 +0600

    Second commit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const user = {
+    name: 'Alex',
+    surname: 'Smith',
+    birthday: '20/04/1993',
+    showMyPublicData: function () {
+        console.log(`${this.name} ${this.surname}`);
+    }
+};
+
+// writable
+// enumerable
+// configurable
